9ab3184101c58ff514e43091231c82403f5f808c
NicolasF425 <nifan42@gmail.com>
2025-09-10 17:02:38 +0200

    Update settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -15,7 +15,11 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "oclettings-latest.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
